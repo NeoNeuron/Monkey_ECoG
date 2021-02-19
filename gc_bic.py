3643a45df0fc88a9a2cac4fef218fadcc21a4bd3
Kyle Chen <kchen513@outlook.com>
2021-02-19 12:01:14 +0800

    Add and modify AIC/BIC for auto-regression model.

--- gc_bic.py
+++ gc_bic.py
@@ -4,36 +4,37 @@ import matplotlib.pyplot as plt
 # plt.style.use('dark_background')
 from GC import create_structure_array
 
-def compute_AIC(data_series):
-    AIC = np.zeros((data_series.shape[1], 50))
-    for i in range(AIC.shape[0]):
+def compute_BIC(data_series):
+    order_max = 50
+    BIC = np.zeros((data_series.shape[1], order_max))
+    for i in range(BIC.shape[0]):
         x = data_series[:,i].copy()
-        for j, order in enumerate(np.arange(AIC.shape[1])+1):
+        for j, order in enumerate(np.arange(BIC.shape[1])+1):
             x_array = create_structure_array(x, order)
             # y_array = create_structure_array(y, order)
             auto_coeff = np.linalg.lstsq(x_array, x[order:],rcond=-1)[0]
             res = x[order:]-x_array @ auto_coeff
-            AIC[i,j] = 2*(order + (x.shape[0]-order)*np.log(res.std()))
-    return AIC
+            BIC[i,j] = np.log(x.shape[0]-order)*order + 2*(x.shape[0]-order)*np.log(res.std())
+    return BIC
 
-def get_AICs(path, force_compute=False):
+def get_BICs(path, force_compute=False):
     data_package = np.load(path + 'preprocessed_data.npz', allow_pickle=True)
-    fname = 'gc_aic.npz'
+    fname = 'gc_bic.npz'
     if os.path.isfile(path+fname) and not force_compute:
-        AICs = np.load(path + fname, allow_pickle=True)
+        BICs = np.load(path + fname, allow_pickle=True)
     else:
-        AICs = {}
+        BICs = {}
         data_series = data_package['data_series_raw']
-        AICs['raw'] = compute_AIC(data_series)
+        BICs['raw'] = compute_BIC(data_series)
         filter_pool = ['delta', 'theta', 'alpha', 'beta', 'gamma', 'high_gamma']
         for band in filter_pool:
             data_series = data_package[f'data_series_{band:s}']
-            AICs[band] = compute_AIC(data_series)
-    return AICs
+            BICs[band] = compute_BIC(data_series)
+    return BICs
 
 if __name__ == '__main__':
     path = 'data_preprocessing_46_region/'
-    AICs = get_AICs(path, False)
+    BICs = get_BICs(path, False)
 
     filter_pool = ['delta', 'theta', 'alpha', 'beta', 'gamma', 'high_gamma']
     fig = plt.figure(figsize=(14,6), dpi=200)
@@ -42,11 +43,11 @@ if __name__ == '__main__':
                                     top=0.69, bottom=0.31) 
     ax = fig.add_subplot(gs_raw[0])
 
-    for aic in AICs['raw']:
-        aic = (aic-aic.min())/(aic.max()-aic.min())
-        ax.plot(np.arange(50)+1, aic, 'navy', alpha=.5)
+    for bic in BICs['raw']:
+        bic = (bic-bic.min())/(bic.max()-bic.min())
+        ax.plot(np.arange(50)+1, bic, 'navy', alpha=.5)
     ax.set_xlabel('order')
-    ax.set_ylabel('Normalized AIC value')
+    ax.set_ylabel('Normalized BIC value')
     ax.set_title('raw')
 
     gs = fig.add_gridspec(nrows=2, ncols=3, 
@@ -54,14 +55,14 @@ if __name__ == '__main__':
                         wspace=0.15)
     axs = np.array([fig.add_subplot(i) for i in gs])
     for band, ax in zip(filter_pool, axs):
-        for aic in AICs[band]:
-            aic = (aic-aic.min())/(aic.max()-aic.min())
-            ax.plot(np.arange(50)+1, aic, 'navy', alpha=.5)
+        for bic in BICs[band]:
+            bic = (bic-bic.min())/(bic.max()-bic.min())
+            ax.plot(np.arange(50)+1, bic, 'navy', alpha=.5)
         ax.set_title(band)
 
-    [axs[i].set_ylabel('Normalized AIC value') for i in (0,3)]
+    [axs[i].set_ylabel('Normalized BIC value') for i in (0,3)]
     [axs[i].set_xlabel('order', fontsize=16) for i in (3,4,5)]
-    plt.savefig(path+'gc_aic.png')
+    plt.savefig(path+'gc_bic.png')
     plt.close()
 
 
