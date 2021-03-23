a9853312a48b74e0755a6e76ed2b684384218221
Kyle Chen <kchen513@outlook.com>
2021-03-23 17:42:23 +0800

    Modify scripts for TDMI band-wise decomposition analysis.

--- tdmi_decomp_com_activate.py
+++ tdmi_decomp_com_activate.py
@@ -17,7 +17,22 @@ with open(path + 'snr_th.pkl', 'rb') as f:
 tdmi_data = np.load(path + 'tdmi_data_long.npz', allow_pickle=True)
 filter_pool = ['delta', 'theta', 'alpha', 'beta', 'gamma', 'high_gamma', 'raw']
 
-mi_data = np.zeros((len(filter_pool), np.sum(off_diag_mask).astype(int)))
+with open(path + 'WA_v3.pkl', 'rb') as f:
+    tdmi_mask_total = pickle.load(f)
+
+seperator = [-6, -5, -4, -3, -2, -1, 0]
+title_set = [
+    "## $w_{ij}>10^{%d}$ " % item for item in seperator
+]
+# for title in title_set:
+title = title_set[0]
+tdmi_sum = np.array([tdmi_mask_total[title][key] for key in filter_pool[:-1]]).sum(0)
+snr_mask_2 = tdmi_sum==6
+
+# triu_mask = np.triu(np.ones_like(weight).astype(bool), k=1)
+
+mi_data = np.zeros((len(filter_pool), np.sum(snr_mask_2).astype(int)))
+
 for i, band in enumerate(filter_pool):
     # mi_data[i,:] = tdmi_data[band][triu_mask,1000]
     tdmi_data_band = MI_stats(tdmi_data[band], 'max')
@@ -25,18 +40,14 @@ for i, band in enumerate(filter_pool):
     snr_matrix = compute_snr_matrix(tdmi_data[band])
     snr_mask = snr_matrix > snr_th[band]
     tdmi_data_band[~snr_mask] = noise_matrix[~snr_mask]
-    mi_data[i,:] = tdmi_data_band[off_diag_mask]
+    mi_data[i,:] = tdmi_data_band[snr_mask_2]
 
-fig, ax = plt.subplots(1,7, figsize=(25,3))
+fig, ax = plt.subplots(7,1, figsize=(3,20), sharex=True)
 for i in range(6):
     ax[i].loglog(mi_data[i,:], mi_data[-1,:], '.k', ms=.4)
     pval = np.polyfit(mi_data[i,:], mi_data[-1,:], deg=1)
     r2 = Linear_R2(mi_data[i,:], mi_data[-1,:], pval)
-    xticks = np.array([mi_data[i,:].min(),mi_data[i,:].max()])
-    print(pval)
-    if xticks[0] == 0:
-        xticks[0] = np.sort(np.unique(mi_data[i,:]))[1]
-    ax[i].loglog(xticks, np.polyval(pval, xticks), '--', color='orange')
+    ax[i].loglog([mi_data[i,:].min(),mi_data[i,:].max()], np.polyval(pval, [mi_data[i,:].min(), mi_data[i,:].max()]), '--', color='orange')
     ax[i].set_xlabel(r'$MI_{band}$')
     ax[i].set_ylabel(r'$MI_{raw}$')
     ax[i].set_title(f'{filter_pool[i]:s}, r={np.sqrt(r2):.3f}')
@@ -54,4 +65,4 @@ r2 = Linear_R2(lsq_lhs, mi_data[-1,:], [1,0])
 ax[-1].loglog([lsq_lhs.min(),lsq_lhs.max()], [lsq_lhs.min(), lsq_lhs.max()], '--', color='orange')
 ax[-1].set_title(f'Least Square Fit: r={np.sqrt(r2):.3f}')
 plt.tight_layout()
-plt.savefig(path + 'tdmi_decomp.png')
+plt.savefig(path + 'tdmi_decomp_com_activate.png')
