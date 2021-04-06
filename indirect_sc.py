076e8f7450f71b3784056db23917208ebd77103a
Kyle Chen <kchen513@outlook.com>
2021-04-06 19:50:05 +0800

    Modify indirect sc ploting style.

--- indirect_sc.py
+++ indirect_sc.py
@@ -5,15 +5,15 @@ plt.rcParams['font.size'] = 20
 from utils.core import EcogTDMI, EcogGC
 from utils.utils import Linear_R2
 def linear_fit(x, y):
-    not_nan_mask = ~np.isnan(x)*~np.isnan(y)
+    not_nan_mask = ~np.isnan(x)*~np.isnan(y)*~np.isinf(x)
     pval = np.polyfit(x[not_nan_mask], y[not_nan_mask], deg=1)
     r = Linear_R2(x, y, pval)**0.5
     return pval, r
 
 # %%
 data = EcogTDMI()
-data.init_data('tdmi_snr_analysis/')
-# data.init_data()
+# data.init_data('tdmi_snr_analysis/')
+data.init_data()
 sc, fc = data.get_sc_fc('ch')
 roi_mask = data.roi_mask.copy()
 
@@ -64,17 +64,54 @@ ax[-1].axis('off')
 plt.tight_layout()
 fig.savefig('Direct_Indirect_MI_distribution.png')
 # %%
-new_sc = sc['raw']+w_degree2_flatten
-fig, ax = plt.subplots(1,1,figsize=(10,8))
-pval,r1 = linear_fit(np.log10(new_sc), np.log10(fc['raw']))
-ax.plot(np.log10(new_sc), np.log10(fc['raw']), '.b', markersize=1)
-ax.plot(np.arange(-6,1), np.polyval(pval, np.arange(-6,1)), 'b', label='Fit all points')
-ax.plot(np.log10(w_degree2_flatten[w_degree2_flatten>0]), np.log10(fc['raw'][w_degree2_flatten>0]), '.r', markersize=1)
-pval,r2 = linear_fit(np.log10(w_degree2_flatten[w_degree2_flatten>0]), np.log10(fc['raw'][w_degree2_flatten>0]))
-ax.plot(np.arange(-6,1), np.polyval(pval, np.arange(-6,1)), 'r', label='Fit indirect pairs')
-ax.set_xlabel(r'$\log_{10}$(SCs)')
-ax.set_ylabel(r'$\log_{10}$(TDMIs)')
-ax.set_title(f'r(blue) = {r1:6.3f}, r(red) = {r2:6.3f}')
-ax.legend()
-plt.savefig('Direct_Indirect_sc_fc_fitting2color.png')
+fig = plt.figure(figsize=(25,14))
+gs = fig.add_gridspec(nrows=2, ncols=4, 
+                      left=0.06, right=0.98, top=0.92, bottom=0.10, 
+                      wspace=0.24, hspace=0.30)
+ax = np.array([fig.add_subplot(i) for i in gs])
+for i, band in enumerate(fc.keys()):
+    # new_sc = sc[band]+w_degree2_flatten
+    pval1,r1 = linear_fit(np.log10(sc[band]), np.log10(fc[band]))
+    ax[i].plot(np.log10(sc[band]), np.log10(fc[band]), '.b', markersize=1)
+    ax[i].plot(np.arange(-6,1), np.polyval(pval1, np.arange(-6,1)), 'b', label='Fit direct pairs')
+    ax[i].plot(np.log10(w_degree2_flatten[w_degree2_flatten>0]), np.log10(fc[band][w_degree2_flatten>0]), '.r', markersize=1)
+    pval2,r2 = linear_fit(np.log10(w_degree2_flatten[w_degree2_flatten>0]), np.log10(fc[band][w_degree2_flatten>0]))
+    ax[i].plot(np.arange(-6,1), np.polyval(pval2, np.arange(-6,1)), 'r', label='Fit indirect pairs')
+    ax[i].set_title(f'r(blue) = {r1:6.3f}, r(red) = {r2:6.3f} \n k(blue) = {pval1[0]:6.3f}, k(red) = {pval2[0]:6.3f}', fontsize=19)
+# plot legend in the empty subplot
+handles, labels = ax[0].get_legend_handles_labels()
+ax[-1].set_visible(True)
+ax[-1].legend(handles, labels, loc=2, fontsize=20)
+ax[-1].axis('off')
+[fig.get_axes()[i].set_ylabel(r'$\log_{10}$(TDMI)') for i in (0,4)]
+[fig.get_axes()[i].set_xlabel('$\log_{10}$(SC)') for i in (3,4,5,6)]
+plt.savefig('Direct_Indirect_sc_fc_fitting2color_mi.png')
+
 # %%
+data = EcogGC()
+data.init_data()
+sc, fc = data.get_sc_fc('ch')
+
+# %%
+fig = plt.figure(figsize=(25,14))
+gs = fig.add_gridspec(nrows=2, ncols=4, 
+                      left=0.06, right=0.98, top=0.92, bottom=0.10, 
+                      wspace=0.24, hspace=0.30)
+ax = np.array([fig.add_subplot(i) for i in gs])
+for i, band in enumerate(fc.keys()):
+    # new_sc = sc[band]+w_degree2_flatten
+    pval1,r1 = linear_fit(np.log10(sc[band]), np.log10(fc[band]))
+    ax[i].plot(np.log10(sc[band]), np.log10(fc[band]), '.b', markersize=1)
+    ax[i].plot(np.arange(-6,1), np.polyval(pval1, np.arange(-6,1)), 'b', label='Fit direct pairs')
+    ax[i].plot(np.log10(w_degree2_flatten[w_degree2_flatten>0]), np.log10(fc[band][w_degree2_flatten>0]), '.r', markersize=1)
+    pval2,r2 = linear_fit(np.log10(w_degree2_flatten[w_degree2_flatten>0]), np.log10(fc[band][w_degree2_flatten>0]))
+    ax[i].plot(np.arange(-6,1), np.polyval(pval2, np.arange(-6,1)), 'r', label='Fit indirect pairs')
+    ax[i].set_title(f'r(blue) = {r1:6.3f}, r(red) = {r2:6.3f} \n k(blue) = {pval1[0]:6.3f}, k(red) = {pval2[0]:6.3f}', fontsize=19)
+# plot legend in the empty subplot
+handles, labels = ax[0].get_legend_handles_labels()
+ax[-1].set_visible(True)
+ax[-1].legend(handles, labels, loc=2, fontsize=20)
+ax[-1].axis('off')
+[fig.get_axes()[i].set_ylabel(r'$\log_{10}$(TDMI)') for i in (0,4)]
+[fig.get_axes()[i].set_xlabel('$\log_{10}$(SC)') for i in (3,4,5,6)]
+plt.savefig('Direct_Indirect_sc_fc_fitting2color_gc.png')
